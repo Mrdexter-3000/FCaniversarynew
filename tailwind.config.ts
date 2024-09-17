5351b2b47883fe011bd20308996376d9dc645c1a
Dilshan Senes <dext3rgraphics@Dilshans-MacBook-Air.local>
2024-09-18 05:14:22 +0530

    text render

---
+++ tailwind.config.ts
@@ -0,0 +1,24 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./pages/**/*.{ts,tsx}",
+    "./components/**/*.{ts,tsx}",
+    "./app/**/*.{ts,tsx}",
+    "./@/**/*.{ts,tsx}",
+    "./src/**/*.{ts,tsx}",
+    // js files primarily because in dist
+    "./node_modules/frames.js/dist/**/*.{ts,tsx,js,css}",
+    "./node_modules/@frames.js/render/dist/*.{ts,tsx,js,css}",
+    "./node_modules/@frames.js/render/dist/**/*.{ts,tsx,js,css}",
+
+    // monorepo weirdness
+    "../../node_modules/frames.js/dist/**/*.{ts,tsx,js,css}",
+    "../../node_modules/@frames.js/render/dist/*.{ts,tsx,js,css}",
+    "../../node_modules/@frames.js/render/dist/**/*.{ts,tsx,js,css}",
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
