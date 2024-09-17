616bf6a56c6ec4c399056492d4fbb7eecc02ee4d
Dilshan Senes <dext3rgraphics@Dilshans-MacBook-Air.local>
2024-09-18 03:07:15 +0530

    env update

--- next.config.js
+++ next.config.js
@@ -2,8 +2,7 @@
 const nextConfig = {
   env: {
     AIRSTACK_API_KEY: process.env.AIRSTACK_API_KEY,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL || 'https://fc-aniversary-v3.vercel.app',
+    APP_URL: process.env.APP_URL || process.env.VERCEL_URL || 'https://fc-aniversary-v3.vercel.app',
   },
 };
-
 export default nextConfig;
