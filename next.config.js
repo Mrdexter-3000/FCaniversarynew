03cb5c9da24cb9f40b7240cdefec79b08dcb1122
Dilshan Senes <dext3rgraphics@Dilshans-MacBook-Air.local>
2024-09-18 02:23:24 +0530

    Update dependencies

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   env: {
     AIRSTACK_API_KEY: process.env.AIRSTACK_API_KEY,
-    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL || 'localhost:3000',
+    NEXT_PUBLIC_APP_URL: process.env.NEXT_PUBLIC_APP_URL || process.env.VERCEL_URL || 'https://fc-aniversary-v3.vercel.app',
   },
 };
 
