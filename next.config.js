60d5f13f5c09d46b1e5c4eb1c43b41b623b62019
Mrdexter-3000 <71649079+Mrdexter-3000@users.noreply.github.com>
2024-09-26 22:49:46 +0530

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,10 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'fc-aniversary-v31-updated-dex-tros-projects.vercel.app'],
+    domains: [
+      'localhost',
+      process.env.VERCEL_URL ? process.env.VERCEL_URL.replace('https://', '') : 'fc-aniversary-v3.vercel.app'
+    ],
   },
   async headers() {
     return [
