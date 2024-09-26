2a877595a78d0f808a354df8ed8eb683ec15d530
Dilshan Senes <dext3rgraphics@Dilshans-MacBook-Air.local>
2024-09-26 21:09:09 +0530

    new gif v2

--- next.config.js
+++ next.config.js
@@ -2,7 +2,7 @@
 const nextConfig = {
   reactStrictMode: true,
   images: {
-    domains: ['localhost', 'fc-aniversary-v31-updated-dex-tros-projects.vercel.app'],
+    domains: ['localhost', 'fc-aniversary-v3.vercel.app'],
   },
   async headers() {
     return [
